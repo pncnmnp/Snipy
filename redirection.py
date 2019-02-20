d2d9210af742a072dfb8c5a0fd6de41f0de43c37
parthparikh <parthparikh1999p@gmail.com>
2019-02-20 23:32:30 +0530

    created a web application

--- redirection.py
+++ redirection.py
@@ -1,10 +1,10 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, request, render_template
 import sqlite3
 import shorten
 
 app = Flask(__name__)
 
-@app.route('/<name>')
+@app.route('/<name>', methods=['POST', 'GET'])
 def direct(name):
 	base = shorten.urlShortner().base
 
@@ -18,9 +18,20 @@ def direct(name):
 
 	return redirect(link, code=302)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-	return ("welcome to test site")
+	error = None
+	if request.method == 'POST':
+		if request.form['query']:
+			s_obj = shorten.urlShortner()
+			url = request.form['query']
+			s_obj.url = url;
+			s_obj.dbFetchStore() # for fetching uid
+			s_obj.shortenUrl()
+			s_obj.dbFetchStore() # for storing shortened link
+
+			return render_template('index.html', name=s_obj.base)
+	return render_template('index.html', error=error)
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
